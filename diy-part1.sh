714b38a2af51ed5107f4d130d6390f88b4e2b99b
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2024-01-16 18:17:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -37,7 +37,7 @@ sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf
 
 git clone https://github.com/kiddin9/openwrt-packages
 shopt -s extglob
-rm -rf openwrt-packages/!(luci-app-turboacc|shortcut-fe|https://github.com/dnsforwarder)
+rm -rf openwrt-packages/!(luci-app-turboacc|shortcut-fe|dnsforwarder)
 cp -r openwrt-packages/{luci-app-turboacc,shortcut-fe,dnsforwarder} package/
 rm -rf openwrt-packages
 
